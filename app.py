b3226e10d881a10e5386173e366c7289512c5191
Elite <joylinechep@kabarak.ac.ke>
2022-03-02 09:39:39 +0300

    First commit

---
+++ app.py
@@ -0,0 +1,17 @@
+#print("Hello world")
+#print("   / | ")
+#print("  /  | ")
+#print(" /   |")
+#print("/____|")
+character_name = "morgan"
+character_age = "25"
+print("There was once named a youung lady called " + character_name + ".")
+print("She was legendary by age 25 ")
+print("Now all the ladies celebrate JJ")
+print("She is a true definition of hardwork and zeal")
+print("what can you do at 25")
+
+
+
+
+ 
