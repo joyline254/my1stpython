90c65b349c30a3318214b3d1b51db09363fbfb1b
Elite <joylinechep@kabarak.ac.ke>
2022-03-04 00:25:49 +0300

    created variables and learnt to get input from user

--- app.py
+++ app.py
@@ -2,16 +2,25 @@
 #print("   / | ")
 #print("  /  | ")
 #print(" /   |")
-#print("/____|")
+# print("/____|")
 character_name = "morgan"
-character_age = "25"
-print("There was once named a youung lady called " + character_name + ".")
-print("She was legendary by age 25 ")
-print("Now all the ladies celebrate JJ")
+character_age = "5"
+print("There was once named \n a young lady called " + character_name + " ,")
+print("She was legendary by age " + character_age)
+character_name = "ADELE"
+character_age = "15"
+print("Now all the ladies celebrate " + character_name + " ")
 print("She is a true definition of hardwork and zeal")
-print("what can you do at 25")
-
-
-
-
- 
+print("what can you do at " + character_age + " ")
+animal = "Wolf"
+print(animal.lower() + " is a cool animal")
+animal="Wolf"
+print(animal.capitalize() + " is a cool animal")
+animal="wolf"
+print(animal.upper().isupper() ) 
+from math import *
+my_num=30.2
+print(ceil(my_num))
+name=input("Enter your name:")
+age=input("Enter your age")
+print("Hello " + name+ "!"+"You are "+age+" years old")
